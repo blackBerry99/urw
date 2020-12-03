696579007e55803e4c077b2291b4093913889b21
mac <mac@MacBook-Pro-mac.local>
2020-12-03 14:43:55 +0200

    new

--- src/js/main.js
+++ src/js/main.js
@@ -10,45 +10,25 @@ const dragover = function(event) {
         }
         event.preventDefault();
     };
+
 const drop = function(event) {
-        event.preventDefault();
-        let imageId = event.dataTransfer.getData("text");
-        event.target.appendChild(document.getElementById(imageId));
+    event.preventDefault();
+    let imageId = event.dataTransfer.getData("text");
+    event.target.appendChild(document.getElementById(imageId));
+    let puzzle = []
+    let puzzleCount = document.querySelectorAll('#js-puzzle .js-puzzle__item').length
+    for (let i = 0; i < puzzleCount; i++) {
+        puzzle.push({
+            cell: document.querySelector(`#cell${i + 1}`),
+            image: document.querySelector(`#image_part_00${i + 1}`)
+        })
+    }
 
-        let cell1 = document.querySelector('#cell1');
-        let img1 = document.querySelector('#image_part_001');
-
-        let cell2 = document.querySelector('#cell2');
-        let img2 = document.querySelector('#image_part_002');
-
-        let cell3 = document.querySelector('#cell3');
-        let img3 = document.querySelector('#image_part_003');
-
-        let cell4 = document.querySelector('#cell4');
-        let img4 = document.querySelector('#image_part_004');
-
-        let cell5 = document.querySelector('#cell5');
-        let img5 = document.querySelector('#image_part_005');
-
-        let cell6 = document.querySelector('#cell6');
-        let img6 = document.querySelector('#image_part_006');
-
-        let cell7 = document.querySelector('#cell7');
-        let img7 = document.querySelector('#image_part_007');
-
-        let cell8 = document.querySelector('#cell8');
-        let img8 = document.querySelector('#image_part_008');
-
-        let cell9 = document.querySelector('#cell9');
-        let img9 = document.querySelector('#image_part_009');
-
-        let contains = cell1.contains(img1) && cell2.contains(img2) && cell3.contains(img3) && cell4.contains(img4) && cell5.contains(img5) && cell6.contains(img6) && cell7.contains(img7) && cell8.contains(img8)  && cell9.contains(img9);
-
-        if (contains) {
-            showAlert.style.display = "flex";
-            yourTime.textContent = time.textContent;
-        }
-    };
+    if (puzzle.every(p => p.cell.contains(p.image))) {
+        showAlert.style.display = "flex";
+        yourTime.textContent = time.textContent;
+    }
+};
 
 const cells = document.getElementsByClassName("puzzle__grid-col");
     Array.from(cells).forEach((element) => {
