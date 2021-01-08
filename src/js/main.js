9e09a410fcc4d038c072aead2a5c0f640ce9eb55
mac <mac@MacBook-Pro-mac.local>
2021-01-08 12:07:36 +0200

    new

--- src/js/main.js
+++ src/js/main.js
@@ -1,50 +1,47 @@
 let productList = document.getElementById("js-product-wrapper");
+let products = document.getElementById("js-product-list");
 var demonstration = {
     category: [
         {
             cName: "swipe/move",
             subCategory: [
                 {
-                subCName: "tinder",
-                subCLinks: [
-                    {
-                    linkName: "Emek",
-                    link: "https://adsil1.com/tools/demonstrations/emek/0917/index.html",
+                    subCName: "tinder",
+                    subCLinks: [
+                        {
+                            linkName: "Emek",
+                            link: "https://adsil1.com/tools/demonstrations/emek/0917/index.html",
+                        },
+                        {
+                            linkName: "swipe Game Cellcom",
+                            link: "https://adsil1.com/tools/demonstration/cellcom/swipe_game/1018/index.html",
+                        },
+                    ]
                 },
-                    {
-                        linkName: "swipe Game Cellcom",
-                        link: "https://adsil1.com/tools/demonstration/cellcom/swipe_game/1018/index.html",
-                    },
-                ]
-            },
-            {
-                subCName: "up down",
-                subCLinks: [{
-                    linkName: "XL",
-                    link: "https://adsil1.com/tools/demonstration/XL/1905/live/index.html",
-
+                {
+                    subCName: "up down",
+                    subCLinks: [
+                        {
+                            linkName: "XL",
+                            link: "https://adsil1.com/tools/demonstration/XL/1905/live/index.html",
+                        },
+                        {
+                            linkName: "Azorim",
+                            link: "http://adsil1.com/tools/demonstration/azorim/0518/index.html",
+                        },
+                    ],
                 },
-                    {
-                        linkName: "Azorim",
-                        link: "http://adsil1.com/tools/demonstration/azorim/0518/index.html",
-
-                    },
-
-                ],
-
-            },
-            {
-                subCName: "moveElement",
-                subCLinks: [{
-                    linkName: "BOX",
-                    link: "http://adsil1.com/tools/demonstration/hashmalBox122016/index.html",
-
-                },
-                    {
-                        linkName: "Yoplait",
-                        link: "http://adsil1.com/tools/demonstration/yopllait/index.html",
-
-                    },
+                {
+                    subCName: "moveElement",
+                    subCLinks: [
+                        {
+                            linkName: "BOX",
+                            link: "http://adsil1.com/tools/demonstration/hashmalBox122016/index.html",
+                        },
+                        {
+                            linkName: "Yoplait",
+                            link: "http://adsil1.com/tools/demonstration/yopllait/index.html",
+                        },
 
                 ],
             },
@@ -97,8 +94,8 @@ var demonstration = {
 }
 
 let categoryItem = ''
-demonstration.category.forEach((c) => {
-    categoryItem += `<div class="category__item" id="js-openCategory" onclick="openCategory()">
+demonstration.category.forEach((c, index) => {
+    categoryItem += `<div class="category__item" onclick="openCategory(${index})">
                     <div class="category__title">${c.cName}</div>
                     <div class="category__sub">
                         <div class="category__sub-item"></div>
@@ -107,18 +104,24 @@ demonstration.category.forEach((c) => {
 
 });
 document.getElementById('js-category-list').innerHTML = categoryItem;
-
-
-// let productItem = ''
-// demonstration.category.subCategory.forEach((subC) => {
-//     productItem += `<div class="product__item">
-//                 <div class="product__item-title">${subC.subCName}</div>
-// <!--                <img src="" alt="" class="product__item-banner">-->
-//             </div>`
-//
-// });
-// document.getElementById('js-product-list').innerHTML = productItem;
-//
-function openCategory() {
+function openCategory(index) {
+    let subCategoryItem = ''
+    demonstration.category[index].subCategory.forEach((sc) => {
+        subCategoryItem += `<div class="category__sub">
+                        <div class="category__sub-item" onclick="openProductList(${index})">${sc.subCName}</div>`
+    })
+    document.getElementById('js-product-wrapper').innerHTML = subCategoryItem
     productList.style.display = "block";
+}
+
+function openProductList(index) {
+    let productItem = ''
+    demonstration.category[index].subCategory[index].subCLinks.forEach((scl) => {
+        productItem += `<div class="product__item">
+                <div class="product__item-title">${scl.linkName}</div>
+                <div class="product__item-link"></div>
+            </div>`
+    })
+    document.getElementById('js-product-list').innerHTML = productItem
+    products.style.display = "block";
 }
